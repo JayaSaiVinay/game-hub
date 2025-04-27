2d2d4967bb9e96fa7b61dcd2559d4dfb46892b27
saivinay <ajsvinay@gmail.com>
2025-04-27 11:00:01 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@chakra-ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <Button colorScheme="blue">Button</Button>
+    </div>
+  );
+};
+
+export default App;
