af0ca100b1842e4159b73c7638c9be1424ce88b0
saivinay <ajsvinay@gmail.com>
2025-04-27 11:11:36 +0530

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from "react";
 import { Button } from "@chakra-ui/button";
-
+import { Grid, GridItem } from "@chakra-ui/layout";
+import { Show } from "@chakra-ui/media-query";
 const App = () => {
   return (
-    <div>
-      <Button colorScheme="blue">Button</Button>
-    </div>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        NavBar
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="yellow">
+          NavBar
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        NavBar
+      </GridItem>
+    </Grid>
   );
 };
 
